de96dc6e997c32f4604f9699fb422b24b7301ed1
Ryan Salvanera <rye@coachrye.com>
2021-08-05 02:51:26 +0800

    Text fields will now duplicate each item in the list or matrix, and add a test for valid and invalid input. Keeping this simple for now

--- main.py
+++ main.py
@@ -1,15 +1,91 @@
+from pprint import pprint
 
 def generate_tests(all_fields):
-    print("Processing ... ")
+    # print(all_fields)
+    the_matrix = []
     for test_field in all_fields:
-        print(test_field["Type"])
         if test_field["Type"] == "Text":
-            print("... This is a text")
+            if len(the_matrix) == 0:
+                the_matrix.append(f"{test_field['Name']} Valid")
+                the_matrix.append(f"{test_field['Name']} Invalid")
+            else:
+                the_new_matrix = []
+                print("The Original Matrix")
+                pprint(the_matrix)
+                print("... ... ")
+                for n in range(0, len(the_matrix)):
+                    print(f"Processing {n+1} of {len(the_matrix)}...")
+                    new_item = []
+                    if isinstance(the_matrix[n], list):
+                        for fields in the_matrix[n]:
+                            new_item.append(fields)
+                    else:
+                        new_item.append(the_matrix[n])
+
+                    new_item.append(f"{test_field['Name']} Valid")
+                    the_new_matrix.append(new_item) 
+
+                    new_item = []
+                    if isinstance(the_matrix[n], list):
+                        for fields in the_matrix[n]:
+                            new_item.append(fields)
+                    else:
+                        new_item.append(the_matrix[n])
+
+                    new_item.append(f"{test_field['Name']} Invalid")
+                    the_new_matrix.append(new_item) 
+
+                the_matrix = the_new_matrix
+                print("The Updated Matrix")
+                pprint(the_matrix)
+                print("... ... ")               
+
         elif test_field["Type"] == "Single":
-            print("... This is a single option")
+            print("... This is a single select")
+            # TODO: Single select fields will duplicate each item, and add one test for each option
         elif test_field["Type"] == "Multiple":
-            print("... This is a multiple option")
-    pass
+            print("... This is a multiple select")
+            # TODO: Multi select fields will duplicate each item, and add one test for each option, and another for all options
+            # TODO: Do we need one more test for testing combinations
+    
+    print(" ... ")
+    print(" ... ")
+    print(" ... ")
+    print("The Complete Matrix")
+    pprint(the_matrix)
+
+# t_v
+# t_i
+
+# t_v + s1
+# t_v + s2
+# t_i + s1
+# t_i + s2
+
+# t_v + s1 + m1
+# t_v + s1 + m2
+# t_v + s1 + m1 + m2
+
+# t_v + s2 + m1
+# t_v + s2 + m2
+# t_v + s2 + m1 + m2
+
+# t_i + s1 + m1
+# t_i + s1 + m2
+# t_i + s1 + m1 + m2
+
+# t_i + s2 + m1
+# t_i + s2 + m2
+# t_i + s2 + m1 + m2
+
+# text_v, single1, multi1
+# text_v, single2, multi2
+# text_v, single1, multi1, multi2
+# text_v, single2, multi1, multi2
+# text_i, single1, multi1 
+# text_i, single2, multi2
+# text_i, single1, multi1, multi2
+# text_i, single2, multi1, multi2
 
 
 field_types = "A. Text\nB. Single Select\nC. Multi Select \nX. No more fields\n"
@@ -42,22 +118,35 @@ while field != "X":
         fields.append(current_field)  
 
 ### TODO: Remove Samples 
-field_name = "Text1"
-min_length = "10"
-max_length = "100"
-current_field = {"Type": "Text", "Name": field_name, "MinLength": min_length, "MaxLength": max_length}
-fields.append(current_field)  
+if 1 == 1:
+    field_name = "Text1"
+    min_length = "10"
+    max_length = "100"
+    current_field = {"Type": "Text", "Name": field_name, "MinLength": min_length, "MaxLength": max_length}
+    fields.append(current_field)  
 
-field_name = "Single1"
-field_options = "single_option1, single_option2, single_option3"
-field_options = field_options.split(",")
-current_field = {"Type": "Single", "Name": field_name, "Options": field_options}
-fields.append(current_field)  
+    field_name = "Text2"
+    min_length = "50"
+    max_length = "500"
+    current_field = {"Type": "Text", "Name": field_name, "MinLength": min_length, "MaxLength": max_length}
+    fields.append(current_field)  
 
-field_name = "Multi1"
-field_options = "multi_option1, multi_option2, multi_option3"
-field_options = field_options.split(",")
-current_field = {"Type": "Multiple", "Name": field_name, "Options": field_options}
-fields.append(current_field)  
+    field_name = "Text3"
+    min_length = "25"
+    max_length = "250"
+    current_field = {"Type": "Text", "Name": field_name, "MinLength": min_length, "MaxLength": max_length}
+    fields.append(current_field)  
+
+    # field_name = "Single1"
+    # field_options = "single_option1, single_option2, single_option3"
+    # field_options = field_options.split(",")
+    # current_field = {"Type": "Single", "Name": field_name, "Options": field_options}
+    # fields.append(current_field)  
+
+    # field_name = "Multi1"
+    # field_options = "multi_option1, multi_option2, multi_option3"
+    # field_options = field_options.split(",")
+    # current_field = {"Type": "Multiple", "Name": field_name, "Options": field_options}
+    # fields.append(current_field)  
 
 generate_tests(fields)
